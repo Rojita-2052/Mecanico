89b5ac0e35c9b5739ed30d7989972df920a24e20
pattyta <patricia.aldunce2052@gmail.com>
2023-06-03 10:09:20 -0400

    se modifica lógica de spinner

--- app/src/main/java/com/example/mecanico/Registro.kt
+++ app/src/main/java/com/example/mecanico/Registro.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.util.Log
+import android.view.View
 
 import android.widget.Button
 import android.widget.TextView
@@ -13,21 +15,56 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 class Registro : AppCompatActivity() {
 
-    private lateinit var editTextPatent: EditText
-    private lateinit var spinnerBrands: Spinner
-    private lateinit var spinnerColors: Spinner
-    private lateinit var editTextDate: EditText
-    private lateinit var editTextMileage: EditText
-    private lateinit var checkBoxService: CheckBox
-    private lateinit var checkBoxMaintenance: CheckBox
-    private lateinit var editTextClientRut: EditText
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+        iniciarSpinnerMarca()
+        iniciarSpinnerColor()
+    }
 
-        spinnerBrands = findViewById(R.id.spinnerBrands)
+    fun iniciarSpinnerMarca(){
+        val opcionMarca = arrayOf("Seleccionar","Hyundai", "Chevrolet", "Susuki", "Nissan", "Chery")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, opcionMarca)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val spinnerMarca = findViewById<Spinner>(R.id.sp_marca)
+        spinnerMarca.adapter = adapter
+    }
+    fun iniciarSpinnerColor() {
+        val opcionColor = arrayOf("Seleccionar","Rojo", "Blanco", "Negro", "Verde", "Gris")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, opcionColor)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val spinnerColor = findViewById<Spinner>(R.id.sp_colores)
+        spinnerColor.adapter = adapter
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       /* spinnerBrands = findViewById(R.id.spinnerBrands)
         spinnerColors = findViewById(R.id.spinnerColors)
 
         val brands = arrayOf("Marca 1", "Marca 2", "Marca 3") // Ejemplo de datos para el spinner de marcas
@@ -39,6 +76,5 @@ class Registro : AppCompatActivity() {
 
         val colorAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, colors)
         colorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinnerColors.adapter = colorAdapter
+        spinnerColors.adapter = colorAdapter*/
     }
-}
