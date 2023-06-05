be50d0fd9851daf1857a8218319beb4738b57373
pattyta <patricia.aldunce2052@gmail.com>
2023-06-04 22:34:08 -0400

    se agrega mejora en validacion de error en formulario de inicio

--- app/src/main/java/com/example/mecanico/Home.kt
+++ app/src/main/java/com/example/mecanico/Home.kt
@@ -28,7 +28,5 @@ class Home : AppCompatActivity() {
              startActivity(intent)
         }
 
-
-
     }
 }
--- app/src/main/java/com/example/mecanico/Inicio.kt
+++ app/src/main/java/com/example/mecanico/Inicio.kt
@@ -1,35 +1,42 @@
 package com.example.mecanico
 
 import android.content.Intent
+import android.opengl.GLES30
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-
 class Inicio : AppCompatActivity() {
-
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
-        val btnIngresar : Button = findViewById(R.id.btn_ingresar)
-
-        val email : TextView = findViewById(R.id.txt_email)
-        val pass : TextView= findViewById<TextView>(R.id.txt_password)
         firebaseAuth = Firebase.auth
-        btnIngresar.setOnClickListener()
-        {
-            if(email.text.isNotEmpty()||pass.text.isNotEmpty()){
-                credenciales(email.text.toString(),pass.text.toString())
-            }else{
-                Toast.makeText(baseContext, "¡Por favor ingresa un email y/o contraseña válido!",Toast.LENGTH_SHORT).show()
-            }
+    }
+
+
+    fun enviar(view: View)
+    {
+        val email : EditText = findViewById(R.id.txt_email)
+        val pass : EditText= findViewById(R.id.txt_password)
+
+        if(email.text.isNotEmpty()||pass.text.isNotEmpty()){
+            credenciales(email.text.toString(),pass.text.toString())
+        }else{
+            email.error = "Campo requerido"
+            pass.error = "Campo requerido"
+            Toast.makeText(baseContext, "¡Por favor ingresa un email y/o contraseña válido!",Toast.LENGTH_LONG).show()
         }
     }
     private fun credenciales(email: String, clave: String){
