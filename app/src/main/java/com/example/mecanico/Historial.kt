a6ec01d4c74b591399db4820767b2ed120168ce8
pedrom29 <pedromatu29@gmail.com>
2023-06-16 23:25:02 -0400

    se desarrolla la vista para el historial de inspecciones

--- app/src/main/java/com/example/mecanico/Historial.kt
+++ app/src/main/java/com/example/mecanico/Historial.kt
@@ -1,32 +1,46 @@
 package com.example.mecanico
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.*
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.get
-import com.example.mecanico.databinding.ActivityHistorialBinding
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-
+import com.google.firebase.database.*
 
 class Historial : AppCompatActivity() {
-    private lateinit var binding: ActivityHistorialBinding
-    private lateinit var datebase: DatabaseReference
+    private lateinit var listViewInspecciones: ListView
+    private lateinit var database: DatabaseReference
+    private lateinit var inspeccionesList: MutableList<Inspeccion>
+    private lateinit var adapter: ArrayAdapter<Inspeccion>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityHistorialBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_historial)
 
+        listViewInspecciones = findViewById(R.id.listViewInspecciones)
+        database = FirebaseDatabase.getInstance().getReference("Inspecciones")
+        inspeccionesList = mutableListOf()
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, inspeccionesList)
+        listViewInspecciones.adapter = adapter
 
+        mostrarInspecciones()
+    }
 
-                val intent= Intent(applicationContext, Home::class.java)
-                startActivity(intent)
-
+    private fun mostrarInspecciones() {
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                inspeccionesList.clear()
+                for (dataSnapshot in snapshot.children) {
+                    val inspeccion = dataSnapshot.getValue(Inspeccion::class.java)
+                    if (inspeccion != null) {
+                        inspeccionesList.add(inspeccion)
+                    }
+                }
+                adapter.notifyDataSetChanged()
             }
-        }
 
-
-
+            override fun onCancelled(error: DatabaseError) {
+                // Manejar el error en caso de que ocurra
+            }
+        })
+    }
+}
