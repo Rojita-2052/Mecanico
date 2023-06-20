32fb7ffaf24bc467d71b21da7a04b8b9b61e4a83
draco_wizard <anibaljara@live.com>
2023-06-20 15:49:26 -0400

    metodo Searchview integrado satisfactoriamente en archivos XML y KT

--- app/src/main/java/com/example/mecanico/Historial.kt
+++ app/src/main/java/com/example/mecanico/Historial.kt
@@ -3,11 +3,13 @@ package com.example.mecanico
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.*
 
 class Historial : AppCompatActivity() {
     private lateinit var listViewInspecciones: ListView
+    private lateinit var searchView: SearchView
     private lateinit var database: DatabaseReference
     private lateinit var inspeccionesList: MutableList<Inspeccion>
     private lateinit var adapter: ArrayAdapter<Inspeccion>
@@ -17,10 +19,23 @@ class Historial : AppCompatActivity() {
         setContentView(R.layout.activity_historial)
 
         listViewInspecciones = findViewById(R.id.listViewInspecciones)
+        searchView = findViewById(R.id.searchView)
         database = FirebaseDatabase.getInstance().getReference("Inspecciones")
         inspeccionesList = mutableListOf()
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, inspeccionesList)
         listViewInspecciones.adapter = adapter
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // Aquí puedes implementar la lógica para buscar en la lista
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                // Aquí puedes implementar la lógica para buscar en la lista a medida que se va escribiendo
+                return false
+            }
+        })
 
         mostrarInspecciones()
     }
