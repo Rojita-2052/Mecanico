6fed19f9b40a5ff96418fbe9b0c4a75d0a5b0017
pedrom29 <pedromatu29@gmail.com>
2023-06-29 22:04:33 -0400

    se corrigen errores en el search para que sea funcional

--- app/src/main/java/com/example/mecanico/Historial.kt
+++ app/src/main/java/com/example/mecanico/Historial.kt
@@ -1,11 +1,10 @@
 package com.example.mecanico
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.*
-import android.widget.SearchView
+import androidx.appcompat.widget.SearchView
 import android.widget.Toast
 
 class Historial : AppCompatActivity() {
@@ -13,45 +12,71 @@ class Historial : AppCompatActivity() {
     private lateinit var inspeccionsArrayList: ArrayList<ObtenerInspecciones>
     private lateinit var database: DatabaseReference
     private lateinit var searchView: SearchView
+    private lateinit var adapter: MyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_historial)
 
-       recyclerView = findViewById(R.id.lista_inspecciones)
-       recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView = findViewById(R.id.lista_inspecciones)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
-       inspeccionsArrayList= arrayListOf()
+        inspeccionsArrayList = arrayListOf()
+        adapter = MyAdapter(inspeccionsArrayList)
+        recyclerView.adapter = adapter
 
-       database = FirebaseDatabase.getInstance().getReference("Inspecciones")
-       database.addValueEventListener(object :ValueEventListener{
-           override fun onDataChange(snapshot: DataSnapshot) {
-               if(snapshot.exists()){
-                   for(dataSnapShot in snapshot.children){
-                       val inspecciones = dataSnapShot.getValue(ObtenerInspecciones::class.java)
-                       if(!inspeccionsArrayList.contains(inspecciones)){
-                           inspeccionsArrayList.add(inspecciones!!)
-                       }
-                   }
-                   recyclerView.adapter = MyAdapter(inspeccionsArrayList)
-               }
-           }
-           override fun onCancelled(error: DatabaseError) {
-               Toast.makeText(this@Historial,error.toString(),Toast.LENGTH_SHORT).show()
-           }
-       })
-        /*searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        database = FirebaseDatabase.getInstance().getReference("Inspecciones")
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    for (dataSnapShot in snapshot.children) {
+                        val inspecciones = dataSnapShot.getValue(ObtenerInspecciones::class.java)
+                        if (!inspeccionsArrayList.contains(inspecciones)) {
+                            inspeccionsArrayList.add(inspecciones!!)
+                        }
+                    }
+                    adapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(
+                    this@Historial,
+                    error.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+
+        searchView = findViewById(R.id.searchView)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
                 searchView.clearFocus()
                 return false
             }
 
             override fun onQueryTextChange(newText: String): Boolean {
-                // Aquí puedes implementar la lógica para buscar a medida que se va escribiendo
-                return false
-            }
-        })*/
+                val filteredList = ArrayList<ObtenerInspecciones>()
 
+                for (inspeccion in inspeccionsArrayList) {
+                    if (inspeccion.nombre?.toLowerCase()?.contains(newText.toLowerCase()) == true ||
+                        inspeccion.patente?.toLowerCase()?.contains(newText.toLowerCase()) == true ||
+                        inspeccion.rut?.toLowerCase()?.contains(newText.toLowerCase()) == true ||
+                        inspeccion.marca?.toLowerCase()?.contains(newText.toLowerCase()) == true ||
+                        inspeccion.motivo?.toLowerCase()?.contains(newText.toLowerCase()) == true ||
+                        inspeccion.kilometraje?.toLowerCase()?.contains(newText.toLowerCase()) == true ||
+                        inspeccion.date?.toLowerCase()?.contains(newText.toLowerCase()) == true ||
+                        inspeccion.colores?.toLowerCase()?.contains(newText.toLowerCase()) == true
+                    ) {
+                        filteredList.add(inspeccion)
+                    }
+                }
+
+                adapter.setData(filteredList)
+                adapter.notifyDataSetChanged()
+
+                return true
+            }
+        })
     }
 }
-
--- app/src/main/java/com/example/mecanico/MyAdapter.kt
+++ app/src/main/java/com/example/mecanico/MyAdapter.kt
@@ -6,36 +6,44 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class MyAdapter(val inspeccionesList: ArrayList<ObtenerInspecciones>):RecyclerView.Adapter<MyAdapter.MyViewHolder>(){
+class MyAdapter(private var inspeccionesList: ArrayList<ObtenerInspecciones>) :
+    RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
-    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-            val patente:TextView = itemView.findViewById(R.id.txtv_patente)
-            val marca:TextView = itemView.findViewById(R.id.txtv_marca)
-            val color:TextView = itemView.findViewById(R.id.txtv_color)
-            val fecha:TextView = itemView.findViewById(R.id.txtv_fecha)
-            val kilometraje:TextView = itemView.findViewById(R.id.txtv_kilometraje)
-            val motivo:TextView = itemView.findViewById(R.id.txtv_motivo)
-            val nombre:TextView = itemView.findViewById(R.id.txtv_nombre)
-            val rut:TextView = itemView.findViewById(R.id.txtv_rut)
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val patente: TextView = itemView.findViewById(R.id.txtv_patente)
+        val marca: TextView = itemView.findViewById(R.id.txtv_marca)
+        val color: TextView = itemView.findViewById(R.id.txtv_color)
+        val fecha: TextView = itemView.findViewById(R.id.txtv_fecha)
+        val kilometraje: TextView = itemView.findViewById(R.id.txtv_kilometraje)
+        val motivo: TextView = itemView.findViewById(R.id.txtv_motivo)
+        val nombre: TextView = itemView.findViewById(R.id.txtv_nombre)
+        val rut: TextView = itemView.findViewById(R.id.txtv_rut)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.lista_item,parent,false)
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.lista_item, parent, false)
         return MyViewHolder(itemView)
-
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.patente.text = inspeccionesList[position].patente
-        holder.marca.text = inspeccionesList[position].marca
-        holder.color.text = inspeccionesList[position].colores
-        holder.fecha.text = inspeccionesList[position].date
-        holder.kilometraje.text = inspeccionesList[position].kilometraje
-        holder.motivo.text = inspeccionesList[position].motivo
-        holder.nombre.text = inspeccionesList[position].nombre
-        holder.rut.text = inspeccionesList[position].rut
+        val inspeccion = inspeccionesList[position]
+        holder.patente.text = inspeccion.patente
+        holder.marca.text = inspeccion.marca
+        holder.color.text = inspeccion.colores
+        holder.fecha.text = inspeccion.date
+        holder.kilometraje.text = inspeccion.kilometraje
+        holder.motivo.text = inspeccion.motivo
+        holder.nombre.text = inspeccion.nombre
+        holder.rut.text = inspeccion.rut
     }
+
     override fun getItemCount(): Int {
         return inspeccionesList.size
+    }
+
+    fun setData(data: ArrayList<ObtenerInspecciones>) {
+        inspeccionesList = data
+        notifyDataSetChanged()
     }
 }
