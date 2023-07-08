d8ab5ba1fcec6731fc77761006a31b3c868c1765
pattyta <patricia.aldunce2052@gmail.com>
2023-07-07 22:39:11 -0400

    se corrige orden de llamada de datos

--- app/src/main/java/com/example/mecanico/ObtenerInspecciones.kt
+++ app/src/main/java/com/example/mecanico/ObtenerInspecciones.kt
@@ -1,12 +1,12 @@
 package com.example.mecanico
 
 data class ObtenerInspecciones(
+    val patente: String? = null,
+    val marca: String? = null,
     val colores: String? = null,
     val date: String? = null,
     val kilometraje: String? = null,
-    val marca: String? = null,
     val motivo: String? = null,
-    val nombre: String? = null,
-    val patente: String? = null,
     val rut: String? = null,
+    val nombre: String? = null,
 )
